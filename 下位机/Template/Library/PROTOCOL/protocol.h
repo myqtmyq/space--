d568015ca678876f05543f9db4fef93c8e1bb8c9
Rian <rn1494278048@outlook.com>
2024-07-26 15:49:58 +0800

    building protocal

--- 下位机/Template/Library/PROTOCOL/protocol.h
+++ 下位机/Template/Library/PROTOCOL/protocol.h
@@ -26,7 +26,7 @@ typedef union
         0x2000,
         0x00,
     };
-    uint8_t data[1];
+    uint8_t fractionizeMode;
   };
 
   struct
@@ -46,7 +46,7 @@ typedef union
         0x2001,
         0x00,
     };
-    uint8_t data[1];
+    uint8_t statiCurrentMode;
   };
 
   struct
@@ -66,7 +66,7 @@ typedef union
         0x2002,
         0x00,
     };
-    uint8_t data[1];
+    uint8_t dynamicCurrentMode;
   };
 
   struct
@@ -86,7 +86,7 @@ typedef union
         0x2010,
         0x00,
     };
-    uint8_t data[1];
+    uint8_t canID;
   };
 
   struct
@@ -108,7 +108,8 @@ typedef union
         0x2011,
         0x00,
     };
-    uint8_t data[4];
+    uint8_t baudRate;
+    uint32_t reserve : 24;
   };
 
   struct
@@ -117,3 +118,223 @@ typedef union
   };
 
 } param;
+
+/* @attention uint16_t
+    reserve must be 0
+    only read*/
+typedef union
+{
+  struct
+  {
+    operation operation_t;
+    wholeIndex wholeIndex_t = {
+        0x6040,
+        0x00,
+    };
+    uint8_t start : 1;
+    uint8_t enable : 1;
+    uint8_t emergencyStop : 1;
+    uint8_t emergencyStopMode : 1;
+    uint8_t faultReset : 1;
+    uint8_t setOrigin : 1;
+    uint8_t setZero : 1;
+    uint8_t dataManagement : 1;
+    uint8_t reserve : 8;
+  };
+
+  struct
+  {
+    uint8_t data[6];
+  };
+
+} controlBit;
+
+/* @attention uint16_t*/
+typedef union
+{
+  struct
+  {
+    operation operation_t;
+    wholeIndex wholeIndex_t = {
+        0x6041,
+        0x00};
+    uint8_t start : 1;
+    uint8_t enable : 1;
+    uint8_t emergencyStop : 1;
+    uint8_t run : 1;
+    uint8_t fault : 1;
+    uint8_t reserve : 11;
+  };
+
+  struct
+  {
+    uint8_t data[6];
+  };
+} statusBit;
+
+/* @attention uint8_t
+    0 = stop
+    1 = speedMode
+    2 = locationMode
+    3 = timeMode
+    4 = originBack
+    5 = IOspeed
+    6 = IOlocation
+    7 = IOtime
+    8 = IOexternPWM*/
+typedef union
+{
+  struct
+  {
+    operation operation_t;
+    wholeIndex wholeIndex_t = {
+        0x2020,
+        0x00,
+    };
+    uint8_t motorMode;
+  };
+
+  struct
+  {
+    uint8_t data[5];
+  };
+
+} motorMode;
+
+/* @attention uint32_t
+    speed: 0 = pulse/s 1 = 0.01 rpm
+    location: 0 = pulse 1 = 0.01 degree
+    time: 0 = ms 1 = 0.01 ml
+    */
+typedef union
+{
+  struct
+  {
+    operation operation_t;
+    wholeIndex wholeIndex_t = {
+        0x2023,
+        0x00,
+    };
+    uint8_t speedUnit;
+    uint8_t locationUnit;
+    uint8_t timeUnit;
+    uint8_t reserve;
+  };
+
+  struct
+  {
+    uint8_t data[8];
+  };
+
+} motorUnit;
+
+/* @attention uint32_t
+    enabled in timeMode
+    the unit is 0.000001 (ml/min)/rpm when sportUnit is 1
+    default cofficient is 1*/
+typedef union
+{
+  struct
+  {
+    operation operation_t;
+    wholeIndex wholeIndex_t = {
+        0x2024,
+        0x00,
+    };
+    uint32_t cofficient;
+  };
+
+  struct
+  {
+    uint8_t data[8];
+  };
+
+} physicalChange_TimeCoficient;
+
+/* @attention int32_t*/
+typedef union
+{
+  struct
+  {
+    operation operation_t;
+    wholeIndex wholeIndex_t = {
+        0x2021,
+        0x00,
+    };
+    int32_t offset;
+  };
+
+  struct
+  {
+    uint8_t data[8];
+  };
+
+} originOffset;
+
+/* @attention uint32_t
+    0 = noError
+    bit0 = loseStep
+    bit1 = stuck
+    bit2 = highTemp(>120 `C)
+    bit3 = lowVoltage(<10V)
+    bit4 = highVoltage(>26V)
+    only read*/
+typedef union
+{
+  struct
+  {
+    operation operation_t;
+    wholeIndex wholeIndex_t = {
+        0x2022,
+        0x00,
+    };
+    uint32_t errorCode;
+  };
+
+  struct
+  {
+    uint8_t data[8];
+  };
+
+} errorCode;
+
+/* @attention uint32_t
+    only in locationMode*/
+typedef union
+{
+  struct
+  {
+    operation operation_t;
+    wholeIndex wholeIndex_t = {
+        0x2030,
+        0x00,
+    };
+    uint32_t maxSpeed;
+  };
+
+  struct
+  {
+    uint8_t data[8];
+  };
+
+} maxSpeed;
+
+/* @attention uint32_t
+ */
+typedef union
+{
+  struct
+  {
+    operation operation_t;
+    wholeIndex wholeIndex_t = {
+        0x2031,
+        0x00,
+    };
+    uint32_t acceleration;
+  };
+
+  struct
+  {
+    uint8_t data[8];
+  };
+};
