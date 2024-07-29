b9a71b2aa8c5ec74ee9e65c04cc86033068bf355
myqtmyq <YMao@2023.ljmu.ac.uk>
2024-07-29 19:21:49 +0800

    update

--- 下位机/Template/Driver/src/Canopen.c
+++ 下位机/Template/Driver/src/Canopen.c
@@ -1,10 +1,6 @@
 #include "Canopen.h"
 
 void CAN_Send_SDO(int id,CtrlWorddef length,uint16_t index,uint8_t subindex,uint32_t data){
-    int cobid=0x600+id;
-    int data[8];
-    data[0]=length;
-    data[1]=index;
 
 }
 
--- 下位机/Template/Driver/src/PUMP.c
+++ 下位机/Template/Driver/src/PUMP.c
@@ -2,11 +2,11 @@
 
 void Pump_Init(void)
 {
-    
+    CAN_SendMessage(controlBit.index,8,*data)
 }
 
 void Pump_Mode(PumpStateTypeDef state){
-    
+
 
     CAN_SendMessage();
 
