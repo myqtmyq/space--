ffe17bab7630316c69da8f00a1b18d03f70f6dd4
Rian <rn1494278048@outlook.com>
2024-08-01 18:40:47 +0800

    add reverse , getCRC functions

--- 下位机/Template/Driver/src/Driver_protocol.c
+++ 下位机/Template/Driver/src/Driver_protocol.c
@@ -34,3 +34,54 @@ uint8_t getOffset(uint16_t index)
     }
   }
 }
+
+/* @param length -> exclusive CRC's 2 bytes*/
+void getCRC16(uint8_t *data, uint8_t length)
+{
+  uint16_t CRC16 = 0xffff;
+  uint16_t ploy = 0x8005;
+  int i = length;
+  while (i--)
+  {
+    CRC16 ^= (*(data + length - i) << 8);
+    for (int i = 0; i < 8; i++)
+    {
+      if (CRC16 & 0x8000)
+      {
+        CRC16 = (CRC16 << 1) ^ ploy;
+      }
+      else
+      {
+        CRC16 << 1;
+      }
+    }
+  }
+  *(data + length) = CRC16 & 0xff;
+  *(data + length + 1) = (CRC16 >> 8) & 0xff;
+}
+
+/* @param length -> exclusive CRC's 2 bytes*/
+void reverse(uint8_t *data, uint8_t length)
+{
+  uint8_t temp;
+  // index
+  temp = *(data + 2);
+  *(data + 2) = *(data + 3);
+  *(data + 3) = temp;
+  // data or register numbers
+  temp = *(data + 4);
+  *(data + 4) = *(data + 5);
+  *(data + 5) = temp;
+  //
+  if (length > 6)
+  {
+    // data
+    int i = (length - 7) / 2;
+    for (int j = 0; j < i; j++)
+    {
+      temp = *(data + 7 + j);
+      *(data + 7 + j) = *(data + length - j);
+      *(data + length - 1 - j) = temp;
+    }
+  }
+}
