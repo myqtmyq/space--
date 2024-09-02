2a5642b2dd0a6dc4b25eadc42f602a55295b822d
Rian-Night <rn1494278048@outlook.com>
2024-09-02 12:59:15 +0800

    none

--- 下位机/Template/Driver/src/BSP_DMA.c
+++ 下位机/Template/Driver/src/BSP_DMA.c
@@ -60,12 +60,10 @@ void spi1TX_DMA_Init()
 {
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
   DMA_InitTypeDef DMA_InitTypeDef_t;
-  DMA_InitTypeDef_t.DMA_DIR = DMA_DIR_PeripheralDST;
   DMA_InitTypeDef_t.DMA_M2M = DMA_M2M_Disable;
   DMA_InitTypeDef_t.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
   DMA_InitTypeDef_t.DMA_MemoryInc = DMA_MemoryInc_Enable;
   DMA_InitTypeDef_t.DMA_Mode = DMA_Mode_Normal;
-  DMA_InitTypeDef_t.DMA_PeripheralBaseAddr = &(SPI1->DR);
   DMA_InitTypeDef_t.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
   DMA_InitTypeDef_t.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
   DMA_InitTypeDef_t.DMA_Priority = DMA_Priority_High;
@@ -73,14 +71,70 @@ void spi1TX_DMA_Init()
   SPI_I2S_DMACmd(SPI1, SPI_I2S_DMAReq_Tx, ENABLE);
 }
 
-void spi1TX_DMA_DNABLE(uint8_t *data, uint16_t length)
+void spi1TX_DMA_ENABLE(uint8_t *data, uint8_t *perAddr,uint16_t length)
 {
   DMA1_Channel3->CNDTR = length;
   DMA1_Channel3->CMAR = data;
+  DMA1_Channel3->CPAR = perAddr;
+  DMA1_Channel3->CCR |= DMA_DIR_PeripheralDST;
   DMA_Cmd(DMA1_Channel3, ENABLE);
 }
 
 void spi1TX_DMA_DISABLE()
 {
   DMA_Cmd(DMA1_Channel3, DISABLE);
+}
+
+void spi1RX_DMA_Init(){
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+  DMA_InitTypeDef DMA_InitTypeDef_t;
+  DMA_InitTypeDef_t.DMA_M2M = DMA_M2M_Disable;
+  DMA_InitTypeDef_t.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+  DMA_InitTypeDef_t.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitTypeDef_t.DMA_Mode = DMA_Mode_Normal;
+  DMA_InitTypeDef_t.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+  DMA_InitTypeDef_t.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitTypeDef_t.DMA_Priority = DMA_Priority_High;
+  DMA_Init(DMA1_Channel2, &DMA_InitTypeDef_t);
+  SPI_I2S_DMACmd(SPI1, SPI_I2S_DMAReq_Rx, ENABLE);
+}
+
+void spi1RX_DMA_ENABLE(uint8_t *data, uint8_t* perAddr, uint16_t length) {
+  DMA1_Channel2->CNDTR = length;
+  DMA1_Channel2->CMAR = data;
+  DMA1_Channel2->CPAR = perAddr;
+  DMA1_Channel2->CCR |= DMA_DIR_PeripheralSRC;
+  DMA_Cmd(DMA1_Channel2, ENABLE);
+}
+
+void spi1RX_DMA_DISABLE() {
+  DMA_Cmd(DMA1_Channel2, DISABLE);
+}
+
+void usart3TX_DMA_Init() {
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+  DMA_InitTypeDef DMA_InitTypeDef_t;
+  DMA_InitTypeDef_t.DMA_M2M = DMA_M2M_Disable;
+  DMA_InitTypeDef_t.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+  DMA_InitTypeDef_t.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitTypeDef_t.DMA_Mode = DMA_Mode_Normal;
+  DMA_InitTypeDef_t.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+  DMA_InitTypeDef_t.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitTypeDef_t.DMA_Priority = DMA_Priority_High;
+  DMA_Init(DMA1_Channel2, &DMA_InitTypeDef_t);
+  USART_DMACmd(USART3, USART_DMAReq_Tx, ENABLE);
+}
+
+void usart3RX_DMA_Init() {
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+  DMA_InitTypeDef DMA_InitTypeDef_t;
+  DMA_InitTypeDef_t.DMA_M2M = DMA_M2M_Disable;
+  DMA_InitTypeDef_t.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+  DMA_InitTypeDef_t.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitTypeDef_t.DMA_Mode = DMA_Mode_Normal;
+  DMA_InitTypeDef_t.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+  DMA_InitTypeDef_t.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitTypeDef_t.DMA_Priority = DMA_Priority_High;
+  DMA_Init(DMA1_Channel2, &DMA_InitTypeDef_t);
+  USART_DMACmd(USART3, USART_DMAReq_Rx, ENABLE);
 }
